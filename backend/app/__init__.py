67b191c30e241cd6fb4694d8c6ec7adbf2c37a9c
Alexis Guanique <guaniqued@gmail.com>
2023-12-08 21:25:37 -0300

    Implementacion de Swagger

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from .db import init_db
 from flask_cors import CORS
+from flask_swagger_ui import get_swaggerui_blueprint
 
 def create_app():
 
@@ -13,6 +14,19 @@ def create_app():
     from . import usuario_bp
 
     app.register_blueprint(usuario_bp.usuario_bp)
+
+    SWAGGER_URL = '/swagger'
+    API_URL = '/static/swagger.json'
+
+    swaggerui_blueprint = get_swaggerui_blueprint(
+        SWAGGER_URL,
+        API_URL,
+        config = {
+            'app_name': 'Sport Club Challenges'
+        }
+    )
+
+    app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
 
 
     @app.route('/')
